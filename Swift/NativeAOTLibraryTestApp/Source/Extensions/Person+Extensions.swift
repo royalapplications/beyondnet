914190bc881bac22574f6710db11d864863951c1
Felix Deimel <office@lemonmojo.com>
2023-02-24 15:45:57 +0100

    Removed dead code

--- Swift/NativeAOTLibraryTestApp/Source/Extensions/Person+Extensions.swift
+++ Swift/NativeAOTLibraryTestApp/Source/Extensions/Person+Extensions.swift
@@ -2,20 +2,6 @@ import Foundation
 import NativeAOTLibraryTest
 
 extension Person {
-	static func createRandom() -> Person {
-		let firstName = randomFirstName()
-		let lastName = randomLastName()
-		let age = randomAge()
-		
-		let person = Person(firstName: firstName,
-							lastName: lastName,
-							age: age)
-		
-		return person
-	}
-}
-
-extension Person {
 	struct RandomData {
 		static let ages: [Int32] = [
 			0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100
