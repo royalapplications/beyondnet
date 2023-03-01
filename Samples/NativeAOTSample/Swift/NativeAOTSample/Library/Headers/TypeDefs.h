7ae902c7060d0e36a8bef11f439887474508e238
Felix Deimel <office@lemonmojo.com>
2023-03-01 13:13:14 +0100

    More structure in header

--- Samples/NativeAOTSample/Swift/NativeAOTSample/Library/Headers/TypeDefs.h
+++ Samples/NativeAOTSample/Swift/NativeAOTSample/Library/Headers/TypeDefs.h
@@ -3,18 +3,7 @@
 
 #import <stdlib.h>
 
-typedef void* System_Object_t;
-typedef void* System_Type_t;
-typedef void* System_AppDomain_t;
-typedef void* System_Exception_t;
-typedef void* System_UnhandledExceptionEventArgs_t;
-
-typedef void (*VoidDelegate_t)(void);
-typedef void (*ContextDelegate_t)(void* context);
-
-typedef void* NativeAOTSample_Person_t;
-typedef void* NativeAOTSample_Company_t;
-
+#pragma mark - Common Enums
 typedef enum __attribute__((enum_extensibility(open))): int32_t {
 	CStatusSuccess = 1,
 	CStatusFailure = -1
@@ -24,5 +13,20 @@ typedef enum __attribute__((enum_extensibility(closed))): uint8_t {
 	CBoolYes = 1,
 	CBoolNo = 0
 } CBool;
+
+#pragma mark - Common Function Pointer Signatures
+typedef void (*VoidDelegate_t)(void);
+typedef void (*ContextDelegate_t)(void* context);
+
+#pragma mark - System
+typedef void* System_Object_t;
+typedef void* System_Type_t;
+typedef void* System_AppDomain_t;
+typedef void* System_Exception_t;
+typedef void* System_UnhandledExceptionEventArgs_t;
+
+#pragma mark - NativeAOTSample
+typedef void* NativeAOTSample_Person_t;
+typedef void* NativeAOTSample_Company_t;
 
 #endif /* NativeAOTSample_TypeDefs_h */
