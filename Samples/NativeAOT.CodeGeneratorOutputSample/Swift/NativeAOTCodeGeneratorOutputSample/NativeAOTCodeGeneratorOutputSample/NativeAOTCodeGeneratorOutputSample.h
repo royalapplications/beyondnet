190e247050f349874f10994b737ecdebd9df8f74
Felix Deimel <felix.deimel@royalapps.com>
2023-03-13 19:13:07 +0100

    + Swift project

---
+++ Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/NativeAOTCodeGeneratorOutputSample/NativeAOTCodeGeneratorOutputSample.h
@@ -0,0 +1,18 @@
+//
+//  NativeAOTCodeGeneratorOutputSample.h
+//  NativeAOTCodeGeneratorOutputSample
+//
+//  Created by Felix Deimel on 13.03.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NativeAOTCodeGeneratorOutputSample.
+FOUNDATION_EXPORT double NativeAOTCodeGeneratorOutputSampleVersionNumber;
+
+//! Project version string for NativeAOTCodeGeneratorOutputSample.
+FOUNDATION_EXPORT const unsigned char NativeAOTCodeGeneratorOutputSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NativeAOTCodeGeneratorOutputSample/PublicHeader.h>
+
+
