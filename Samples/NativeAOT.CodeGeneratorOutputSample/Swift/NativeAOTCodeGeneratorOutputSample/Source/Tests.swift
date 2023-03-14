11bfcd22d1043a905c5512c65c49dc04694d2143
Felix Deimel <felix.deimel@royalapps.com>
2023-03-14 08:54:35 +0100

    Tests

--- Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests.swift
+++ Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests.swift
@@ -23,6 +23,97 @@ final class NativeAOTCodeGeneratorOutputSampleTests: XCTestCase {
             NativeAOT_CodeGeneratorInputSample_TestClass_Destroy(testClass)
         }
         
+        let testClassType = System_Object_GetType(testClass,
+                                                  &exception)
+        
+        guard exception == nil else {
+            XCTFail("GetType should not throw")
+            
+            return
+        }
+        
+        guard let testClassType else {
+            XCTFail("GetType should return a type object")
+            
+            return
+        }
+        
+        defer {
+            System_Object_Destroy(testClassType)
+        }
+        
+        let systemObjectTypeName = "System.Object"
+        
+        var systemObjectType: System_Type_t?
+        
+        systemObjectTypeName.withCString { systemObjectTypeNameC in
+            systemObjectType = System_Type_GetType(systemObjectTypeNameC,
+                                                   .yes,
+                                                   .no,
+                                                   &exception)
+        }
+        
+        guard exception == nil else {
+            XCTFail("System.Type.GetType should not throw")
+            
+            return
+        }
+        
+        guard let systemObjectType else {
+            XCTFail("System.Type.GetType should return a type object")
+            
+            return
+        }
+        
+        defer {
+            System_Object_Destroy(systemObjectType)
+        }
+        
+        let systemObjectTypeNameC = System_Type_ToString(systemObjectType,
+                                                         &exception)
+        
+        guard exception == nil else {
+            XCTFail("System.Type.ToString should not throw")
+            
+            return
+        }
+        
+        guard let systemObjectTypeNameC else {
+            XCTFail("System.Type.ToString should return an instance of a C string")
+            
+            return
+        }
+        
+        let retrievedSystemObjectTypeName = String(cString: systemObjectTypeNameC)
+        
+        systemObjectTypeNameC.deallocate()
+        
+        XCTAssertEqual(systemObjectTypeName, retrievedSystemObjectTypeName)
+        
+        let isTestClassAssignableToSystemObject = System_Type_IsAssignableTo(testClassType,
+                                                                             systemObjectType,
+                                                                             &exception)
+        
+        guard exception == nil else {
+            XCTFail("IsAssignableTo should not throw")
+            
+            return
+        }
+        
+        XCTAssertEqual(CBool.yes, isTestClassAssignableToSystemObject)
+        
+        let isSystemObjectAssignableToTestClass = System_Type_IsAssignableTo(systemObjectType,
+                                                                             testClassType,
+                                                                             &exception)
+        
+        guard exception == nil else {
+            XCTFail("IsAssignableTo should not throw")
+            
+            return
+        }
+        
+        XCTAssertEqual(CBool.no, isSystemObjectAssignableToTestClass)
+        
         NativeAOT_CodeGeneratorInputSample_TestClass_SayHello(testClass,
                                                               &exception)
         
@@ -58,6 +149,8 @@ final class NativeAOTCodeGeneratorOutputSampleTests: XCTestCase {
         }
         
         let helloString = String(cString: hello)
+        
+        hello.deallocate()
         
         XCTAssertEqual("Hello", helloString)
         
--- Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/TypeDefinitions.h
+++ Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/TypeDefinitions.h
@@ -1,25 +1,65 @@
 #ifndef TypeDefinitions_h
 #define TypeDefinitions_h
 
+#import <stdlib.h>
+
+#pragma mark - Common Enums
+typedef enum __attribute__((enum_extensibility(closed))): uint8_t {
+    CBoolYes = 1,
+    CBoolNo = 0
+} CBool;
+
+typedef void* System_Object_t;
+typedef void* System_Type_t;
 typedef void* System_Exception_t;
+
 typedef void* NativeAOT_CodeGeneratorInputSample_TestClass_t;
 
-void* NativeAOT_CodeGeneratorInputSample_TestClass_Create(System_Exception_t* outException);
-void* NativeAOT_CodeGeneratorInputSample_TestClass_Destroy(NativeAOT_CodeGeneratorInputSample_TestClass_t self);
+void
+System_Object_Destroy(System_Object_t self);
 
-void NativeAOT_CodeGeneratorInputSample_TestClass_SayHello(NativeAOT_CodeGeneratorInputSample_TestClass_t self,
-                                                           System_Exception_t* outException);
+System_Type_t
+System_Type_GetType(const char* typeName,
+                    CBool throwOnError,
+                    CBool ignoreCase,
+                    System_Exception_t* outException);
 
-void NativeAOT_CodeGeneratorInputSample_TestClass_SayHello1(NativeAOT_CodeGeneratorInputSample_TestClass_t self,
-                                                            const char* name,
-                                                            System_Exception_t* outException);
+CBool
+System_Type_IsAssignableTo(System_Type_t self,
+                           System_Type_t targetType,
+                           System_Exception_t* outException);
 
-const char* NativeAOT_CodeGeneratorInputSample_TestClass_GetHello(NativeAOT_CodeGeneratorInputSample_TestClass_t self,
-                                                                  System_Exception_t* outException);
+const char*
+System_Type_ToString(System_Type_t self,
+                     System_Exception_t* outException);
 
-int NativeAOT_CodeGeneratorInputSample_TestClass_Add(NativeAOT_CodeGeneratorInputSample_TestClass_t self,
-                                                     int number1,
-                                                     int number2,
-                                                     System_Exception_t* outException);
+System_Type_t
+System_Object_GetType(System_Object_t self,
+                      System_Exception_t* outException);
+
+NativeAOT_CodeGeneratorInputSample_TestClass_t
+NativeAOT_CodeGeneratorInputSample_TestClass_Create(System_Exception_t* outException);
+
+void
+NativeAOT_CodeGeneratorInputSample_TestClass_Destroy(NativeAOT_CodeGeneratorInputSample_TestClass_t self);
+
+void
+NativeAOT_CodeGeneratorInputSample_TestClass_SayHello(NativeAOT_CodeGeneratorInputSample_TestClass_t self,
+                                                      System_Exception_t* outException);
+
+void
+NativeAOT_CodeGeneratorInputSample_TestClass_SayHello1(NativeAOT_CodeGeneratorInputSample_TestClass_t self,
+                                                       const char* name,
+                                                       System_Exception_t* outException);
+
+const char*
+NativeAOT_CodeGeneratorInputSample_TestClass_GetHello(NativeAOT_CodeGeneratorInputSample_TestClass_t self,
+                                                      System_Exception_t* outException);
+
+int
+NativeAOT_CodeGeneratorInputSample_TestClass_Add(NativeAOT_CodeGeneratorInputSample_TestClass_t self,
+                                                 int number1,
+                                                 int number2,
+                                                 System_Exception_t* outException);
 
 #endif /* TypeDefinitions_h */
