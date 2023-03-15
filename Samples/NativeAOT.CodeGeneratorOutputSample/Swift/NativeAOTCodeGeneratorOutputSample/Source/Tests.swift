83136383167b50067548f0d2b0db4640ab8d68ba
Felix Deimel <felix.deimel@royalapps.com>
2023-03-15 11:26:52 +0100

    String Tests

--- Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests.swift
+++ Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests.swift
@@ -566,6 +566,58 @@ final class NativeAOTCodeGeneratorOutputSampleTests: XCTestCase {
         XCTAssertEqual("00000000-0000-0000-0000-000000000000", emptyGuidString)
     }
     
+    func testString() {
+        var exception: System_Exception_t?
+        
+        guard let emptyStringC = System_String_Empty_Get() else {
+            XCTFail("System.String.Empty should return an empty string")
+            
+            return
+        }
+        
+        let emptyString = String(cString: emptyStringC)
+        XCTAssertTrue(emptyString.isEmpty)
+        
+        let isNullOrEmpty = System_String_IsNullOrEmpty(emptyStringC,
+                                                        &exception)
+        
+        guard exception == nil else {
+            XCTFail("System.String.IsNullOrEmpty should not throw")
+            
+            return
+        }
+        
+        XCTAssertEqual(isNullOrEmpty, .yes)
+        
+        let isNullOrWhiteSpace = System_String_IsNullOrWhiteSpace(emptyStringC,
+                                                                  &exception)
+        
+        guard exception == nil else {
+            XCTFail("System.String.IsNullOrWhiteSpace should not throw")
+            
+            return
+        }
+        
+        XCTAssertEqual(isNullOrWhiteSpace, .yes)
+        
+        let nonEmptyString = "Hello World!"
+        
+        var isNonEmptyStringNullOrEmpty = CBool.yes
+        
+        nonEmptyString.withCString { nonEmptyStringC in
+            isNonEmptyStringNullOrEmpty = System_String_IsNullOrEmpty(nonEmptyStringC,
+                                                                      &exception)
+        }
+        
+        guard exception == nil else {
+            XCTFail("System.String.IsNullOrEmpty should not throw")
+            
+            return
+        }
+        
+        XCTAssertEqual(isNonEmptyStringNullOrEmpty, .no)
+    }
+    
     func testEnum() {
         var exception: System_Exception_t?
         
