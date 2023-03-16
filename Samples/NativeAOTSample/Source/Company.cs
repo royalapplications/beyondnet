10618090b7a5d18cb11fb3fdcc0a172cd5a1cd02
Felix Deimel <office@lemonmojo.com>
2023-03-16 15:42:41 +0100

    Remove unused usings

--- Samples/NativeAOTSample/Source/Company.cs
+++ Samples/NativeAOTSample/Source/Company.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
-using NativeAOT.Core;
 
 namespace NativeAOTSample;
 
--- Samples/NativeAOTSample/Source/Person.cs
+++ Samples/NativeAOTSample/Source/Person.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Text.Json.Serialization;
-using NativeAOT.Core;
 
 namespace NativeAOTSample;
 
