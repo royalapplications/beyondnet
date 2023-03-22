f83338b08b845243a84fd1d8c4eef27b99b17df8
Felix Deimel <felix.deimel@royalapps.com>
2023-03-22 20:45:50 +0400

    System.Exception Tests

---
+++ Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests/System/SystemExceptionTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+import NativeAOTCodeGeneratorOutputSample
+
+final class SystemExceptionTests: XCTestCase {
+    func testSystemException() {
+        var exception: System_Exception_t?
+        
+        let exceptionMessage = "I'm a nice exception"
+        
+        var createdException: System_Exception_t?
+        
+        exceptionMessage.withCString { exceptionMessageC in
+            createdException = System_Exception_Create1(exceptionMessageC,
+                                                        &exception)
+        }
+        
+        guard let createdException,
+              exception == nil else {
+            XCTFail("System.Exception ctor should not throw and return an instance")
+            
+            return
+        }
+        
+        defer { System_Exception_Destroy(createdException) }
+        
+        guard let retrievedExceptionMessageC = System_Exception_Message_Get(createdException,
+                                                                            &exception),
+              exception == nil else {
+            XCTFail("System.Exception.Message getter should not throw and return an instance of a C string")
+            
+            return
+        }
+        
+        defer { retrievedExceptionMessageC.deallocate() }
+        
+        let retrievedExceptionMessage = String(String(cString: retrievedExceptionMessageC))
+        
+        XCTAssertEqual(exceptionMessage, retrievedExceptionMessage)
+    }
+}
