cd5ed36a389c6185d46a978d5e18d72de6553179
Felix Deimel <office@lemonmojo.com>
2023-04-03 15:48:34 +0200

    Tests for Base64 encoding/decoding

--- Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests/System/SystemConvertTests.swift
+++ Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests/System/SystemConvertTests.swift
@@ -34,4 +34,87 @@ final class SystemConvertTests: XCTestCase {
 					   System_Convert_ToUInt6415("nonsense", &exception))
 		XCTAssertNotNil(exception)
 	}
+	
+	func testBase64Conversion() {
+		var exception: System_Exception_t?
+		
+		let text = "Hello World!"
+		
+		guard let utf8Encoding = System_Text_Encoding_UTF8_Get(&exception),
+			  exception == nil else {
+			XCTFail("System.Text.Encoding.UTF8 getter should not throw and return an instance")
+			
+			return
+		}
+		
+		defer { System_Text_Encoding_Destroy(utf8Encoding) }
+		
+		guard let textBytes = System_Text_Encoding_GetBytes3(utf8Encoding,
+															 text,
+															 &exception),
+			  exception == nil else {
+			XCTFail("System.Text.Encoding.GetBytes should not throw and return an instance")
+			
+			return
+		}
+		
+		defer { System_Array_Destroy(textBytes) }
+		
+		guard let textAsBase64StringC = System_Convert_ToBase64String(textBytes,
+																	  &exception),
+			  exception == nil else {
+			XCTFail("System.Convert.ToBase64String should not throw and return an instance of a C String")
+			
+			return
+		}
+		
+		defer { textAsBase64StringC.deallocate() }
+		
+		let textAsBase64String = String(cString: textAsBase64StringC)
+		
+		guard let textAsBase64Data = textAsBase64String.data(using: .utf8) else {
+			XCTFail("Failed to convert base64 string to data")
+			
+			return
+		}
+		
+		guard let textData = Data(base64Encoded: textAsBase64Data) else {
+			XCTFail("Failed to decode base64 encoded data")
+			
+			return
+		}
+		
+		guard let decodedText = String(data: textData, encoding: .utf8) else {
+			XCTFail("Failed to decode text data to string")
+			
+			return
+		}
+		
+		XCTAssertEqual(text, decodedText)
+		
+		guard let textBytesRet = System_Convert_FromBase64String(textAsBase64String,
+																 &exception),
+			  exception == nil else {
+			XCTFail("System.Convert.FromBase64String should not throw and return an instance")
+			
+			return
+		}
+		
+		defer { System_Array_Destroy(textBytesRet) }
+		
+		guard let textRetC = System_Text_Encoding_GetString(utf8Encoding,
+															textBytesRet,
+															&exception),
+			  exception == nil else {
+			XCTFail("System.Text.Encoding.GetString should not throw and return an instance of a C String")
+			
+			return
+		}
+		
+		defer { textRetC.deallocate() }
+		
+		let textRet = String(cString: textRetC)
+		
+		XCTAssertEqual(text, textRet)
+	}
 }
