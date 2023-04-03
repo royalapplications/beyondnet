e5ed8d4b41040e1a59af3072e6e434926e6d824e
Felix Deimel <office@lemonmojo.com>
2023-04-03 13:00:24 +0200

    Destructor test in System.Action tests

--- Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests/System/SystemActionTests.swift
+++ Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests/System/SystemActionTests.swift
@@ -4,12 +4,15 @@ import NativeAOTCodeGeneratorOutputSample
 final class SystemActionTests: XCTestCase {
     class Context {
         var numberOfTimesCalled = 0
+		var numberOfTimesDestructorCalled = 0
     }
     
     func testSystemAction() {
+		var exception: System_Exception_t?
+		
         let swiftyContext = Context()
         let contextBox = NativeBox(swiftyContext)
-        let context = contextBox.unretainedPointer()
+        let context = contextBox.retainedPointer()
         
         let cFunction: System_Action_CFunction_t = { innerContext in
             guard let innerContext else {
@@ -23,12 +26,25 @@ final class SystemActionTests: XCTestCase {
             
             innerSwiftyContext.numberOfTimesCalled += 1
         }
+		
+		let cDestructorFunction: System_Action_CDestructorFunction_t = { innerContext in
+			guard let innerContext else {
+				XCTFail("Context is nil")
+				
+				return
+			}
+			
+			let innerContextBox = NativeBox<Context>.fromPointer(innerContext)
+			let innerSwiftyContext = innerContextBox.value
+			
+			innerSwiftyContext.numberOfTimesDestructorCalled += 1
+			
+			innerContextBox.release(innerContext)
+		}
         
         let action = System_Action_Create(context,
                                           cFunction,
-                                          nil)
-        
-        defer { System_Action_Destroy(action) }
+                                          cDestructorFunction)
         
         XCTAssertEqual(0, swiftyContext.numberOfTimesCalled)
         
@@ -39,5 +55,15 @@ final class SystemActionTests: XCTestCase {
         System_Action_Invoke(action)
         System_Action_Invoke(action)
         XCTAssertEqual(4, swiftyContext.numberOfTimesCalled)
+		
+		System_Action_Destroy(action)
+		
+		System_GC_Collect1(&exception)
+		XCTAssertNil(exception)
+
+		System_GC_WaitForPendingFinalizers(&exception)
+		XCTAssertNil(exception)
+		
+		XCTAssertEqual(1, swiftyContext.numberOfTimesDestructorCalled)
     }
 }
