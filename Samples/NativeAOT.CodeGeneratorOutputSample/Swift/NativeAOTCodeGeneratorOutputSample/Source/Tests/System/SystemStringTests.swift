f49ee9831ca214d0c1aa3ca1fa53198276390880
Felix Deimel <office@lemonmojo.com>
2023-04-04 15:51:14 +0200

    String split test

--- Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests/System/SystemStringTests.swift
+++ Samples/NativeAOT.CodeGeneratorOutputSample/Swift/NativeAOTCodeGeneratorOutputSample/Source/Tests/System/SystemStringTests.swift
@@ -104,4 +104,124 @@ final class SystemStringTests: XCTestCase {
 			return
 		}
     }
+	
+	func testStringReplace() {
+		var exception: System_Exception_t?
+		
+		let hello = "Hello"
+		let otherPart = " World 😀"
+		let originalString = "\(hello)\(otherPart)"
+		
+		let expectedString = originalString.replacingOccurrences(of: hello, with: "")
+		
+		guard let replacedStringC = System_String_Replace3(originalString,
+														   hello,
+														   "",
+														   &exception),
+			  exception == nil else {
+			XCTFail("System.String.Replace should not throw and return an instance of a C String")
+			
+			return
+		}
+		
+		defer { replacedStringC.deallocate() }
+		
+		let replacedString = String(cString: replacedStringC)
+		
+		XCTAssertEqual(expectedString, replacedString)
+	}
+	
+	func testStringSubstring() {
+		var exception: System_Exception_t?
+		
+		let string = "Hello World 😀"
+		let needle = "World"
+		
+		let expectedIndex: Int32 = 6
+		
+		let index = System_String_IndexOf4(string,
+										   needle,
+										   &exception)
+		
+		XCTAssertNil(exception)
+		XCTAssertEqual(expectedIndex, index)
+	}
+	
+	func testStringSplit() {
+		var exception: System_Exception_t?
+		
+		let components = [
+			"1. First",
+			" 2. Second",
+			"",
+			"3. Third "
+		]
+		
+		var cleanedComponents = [String]()
+		
+		for component in components {
+			let trimmedComponent = component.trimmingCharacters(in: .whitespacesAndNewlines)
+			
+			guard !trimmedComponent.isEmpty else {
+				continue
+			}
+			
+			cleanedComponents.append(trimmedComponent)
+		}
+		
+		let separator = ";"
+		
+		let string = components.joined(separator: separator)
+		
+		guard let options: System_StringSplitOptions = .init(rawValue: System_StringSplitOptions.removeEmptyEntries.rawValue | System_StringSplitOptions.trimEntries.rawValue) else {
+			XCTFail("Failed to create string split options")
+			
+			return
+		}
+		
+		guard let split = System_String_Split6(string,
+											   separator,
+											   options,
+											   &exception),
+			  exception == nil else {
+			XCTFail("System.String.Split should not throw and return an instance")
+			
+			return
+		}
+		
+		defer { System_Array_Destroy(split) }
+		
+		let length = System_Array_Length_Get(split,
+											 &exception)
+		
+		XCTAssertNil(exception)
+		XCTAssertEqual(cleanedComponents.count, .init(length))
+		
+		for (idx, component) in cleanedComponents.enumerated() {
+			guard let componentRetObj = System_Array_GetValue1(split,
+															   .init(idx),
+															   &exception),
+				  exception == nil else {
+				XCTFail("System.Array.GetValue should not throw and return an instance")
+				
+				return
+			}
+			
+			defer { System_Object_Destroy(componentRetObj) }
+			
+			guard let componentRetC = System_Object_ToString(componentRetObj,
+															 &exception),
+				  exception == nil else {
+				XCTFail("System.Object.ToString should not throw and return an instance of a C String")
+				
+				return
+			}
+			
+			defer { componentRetC.deallocate() }
+			
+			let componentRet = String(cString: componentRetC)
+			
+			XCTAssertEqual(component, componentRet)
+		}
+	}
 }
