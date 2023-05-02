33b20c175758e8f2c382063ce52a1cb120498352
Felix Deimel <office@lemonmojo.com>
2023-05-02 13:40:49 +0200

    Small C sample

---
+++ Samples/NativeAOT.CodeGeneratorOutputSample/C/TypeDefinitions.h
@@ -0,0 +1,1 @@
+../Swift/NativeAOTCodeGeneratorOutputSample/Source/TypeDefinitions.h
---
+++ Samples/NativeAOT.CodeGeneratorOutputSample/C/newguid.c
@@ -0,0 +1,39 @@
+#import <stdio.h>
+#import "TypeDefinitions.h"
+
+int main(void) {
+	System_Exception_t exception;
+	System_Guid_t guid = System_Guid_NewGuid(&exception);
+
+	if (guid == NULL ||
+		exception != NULL) {
+		printf("An error occurred while creating a new System.Guid.\n");
+
+		return 1;
+	}
+
+	System_String_t guidStringDN = System_Guid_ToString(guid, &exception);
+
+	if (guidStringDN == NULL ||
+		exception != NULL) {
+		printf("An error occurred while converting a System.Guid to a System.String.\n");
+
+		return 1;
+	}
+
+	const char* guidString = DNStringToC(guidStringDN);
+
+	if (guidString == NULL) {
+		printf("An error occurred while converting a System.String to a char*.\n");
+
+		return 1;
+	}
+
+	printf("%s\n", guidString);
+
+	free((void*)guidString);
+	System_String_Destroy(guidStringDN);
+	System_Guid_Destroy(guid);
+
+	return 0;
+}
