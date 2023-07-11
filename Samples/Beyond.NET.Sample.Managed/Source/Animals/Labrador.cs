086f2d967e02060e5a38c13f8fd4818e0b4ea197
Felix Deimel <felix@lemonmojo.com>
2023-07-11 09:03:45 +0200

    Add a labrador to test classes

---
+++ Samples/Beyond.NET.Sample.Managed/Source/Animals/Labrador.cs
@@ -0,0 +1,10 @@
+namespace Beyond.NET.Sample;
+
+public class Labrador: Dog
+{
+    public const string LabradorName = "Labrador";
+    
+    public override string Name => LabradorName;
+    
+    public Labrador() { }
+}
