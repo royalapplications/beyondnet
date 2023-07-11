4c2bebe2d8f1acdd5ca781536d6f6413b79e366c
Felix Deimel <felix@lemonmojo.com>
2023-07-11 11:57:25 +0200

    Additional comments in transform.c

--- Samples/Beyond.NET.Sample.C/transform.c
+++ Samples/Beyond.NET.Sample.C/transform.c
@@ -4,24 +4,33 @@
 
 #include "Generated_C.h"
 
+// We use this as the "context" for our .NET delegate
 struct Transformer {
 	int uppercase;
 };
+
+// Declare a typedef for our Transformer struct
 typedef struct Transformer* Transformer_t;
 
+// Creates an instance of a Transformer and configures it
 Transformer_t transformer_create(
 	int uppercase // Pass 0 to lowercase or 1 to uppercase
 ) {
+	// Allocate memory for the transformer context
 	Transformer_t transformer = (Transformer_t)malloc(sizeof(Transformer_t));
+
+	// Configure it
 	transformer->uppercase = uppercase;
 
 	return transformer;
 }
 
+// Destroys an instance of a Transformer
 void transformer_destroy(
 	void* context // The context (Transformer_t)
 ) {
 	if (!context) {
+		// Nothing to do
 		return;
 	}
 
@@ -30,15 +39,18 @@ void transformer_destroy(
 
 	printf("Destroying transformer\n");
 
+	// Free the memory we previously allocated in transformer_create
 	free(transformer);
 }
 
+// Does the actual transformation; Note that the signature must match the one of our .NET StringTransformerDelegate + a context as the first parameter
 System_String_t transformer_transform(
 	void* context, // The context (Transformer_t)
 	System_String_t string // The string to transform
 ) {
 	if (!context ||
-		!string) { // Nothing to do
+		!string) {
+		// Nothing to do
 		return NULL;
 	}
 
@@ -51,11 +63,13 @@ System_String_t transformer_transform(
 	System_String_t transformedSystemString;
 	
 	if (shouldUppercase) { // Uppercase
+		// Call the .NET System.String.ToUpper method
 		transformedSystemString = System_String_ToUpper(
 			string,
 			NULL // TODO: Error handling
 		);
 	} else { // Lowercase
+		// Call the .NET System.String.ToLower method
 		transformedSystemString = System_String_ToLower(
 			string,
 			NULL // TODO: Error handling
@@ -69,6 +83,7 @@ System_String_t transformer_transform(
 	return transformedSystemString;
 }
 
+// Entry point of the program
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		printf("Error: No input specified. Please provide a string to transform as the first and only argument.\n");
@@ -76,6 +91,7 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	// Get the original string from CLI parameters
 	const char* originalCString = argv[1];
 
 	// Convert the C String to a .NET System.String
@@ -106,16 +122,17 @@ int main(int argc, char *argv[]) {
 	const char* transformedCString = DNStringToC(transformedSystemString);
 
 	// Print the original and the transformed strings
-	printf("Original: %s\n", originalCString);
-	printf("Transformed: %s\n", transformedCString);
+	printf("Original: %s\nTransformed: %s\n", originalCString, transformedCString);
 
 	// Clean up
 	System_String_Destroy(originalSystemString);
 	System_String_Destroy(transformedSystemString);
 	free((void*)transformedCString);
+
+	// While we don't need the delegate object here anymore and thus can destroy it, .NET might still hold a reference to it
 	Beyond_NET_Sample_Transformer_StringTransformerDelegate_Destroy(stringTransformerDelegate);
 
-	// Force the GC to collect and call our delegate destructor function
+	// Force the GC to collect now which will cause our delegate destructor function (transformer_destroy) to be called
 	System_GC_Collect_1(NULL);
 	System_GC_WaitForPendingFinalizers(NULL);
 
