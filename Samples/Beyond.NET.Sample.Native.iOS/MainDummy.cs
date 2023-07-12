8337e425b99ffff191609fb3c65d68914964d911
Felix Deimel <felix@lemonmojo.com>
2023-07-12 15:07:45 +0200

    Add .NET part of iOS sample

---
+++ Samples/Beyond.NET.Sample.Native.iOS/MainDummy.cs
@@ -0,0 +1,7 @@
+namespace __MainDummyNamespace;
+
+// TODO: This is a temporary workaround to allow the .NET compiler to generate an iOS native dylib
+public static class __MainDummyClass
+{
+	public static void Main() { }
+}
