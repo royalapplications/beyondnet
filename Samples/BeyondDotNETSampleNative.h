c33079ba4ca076c4dceeaaef42f8f1dd7fdf89d2
Felix Deimel <felix.deimel@royalapps.com>
2023-07-14 20:15:15 +0200

    Generating Swift build works

--- Samples/BeyondDotNETSampleNative.h
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for BeyondDotNETSampleNative.
-FOUNDATION_EXPORT double BeyondDotNETSampleNativeVersionNumber;
-
-//! Project version string for BeyondDotNETSampleNative.
-FOUNDATION_EXPORT const unsigned char BeyondDotNETSampleNativeVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <BeyondDotNETSampleNative/PublicHeader.h>
