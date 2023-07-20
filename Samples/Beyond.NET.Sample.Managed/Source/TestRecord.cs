47516dcf02d59e019ea27e420764e6153911214f
Felix Deimel <felix@lemonmojo.com>
2023-07-20 16:24:16 +0200

    TODO: Test C# Records

---
+++ Samples/Beyond.NET.Sample.Managed/Source/TestRecord.cs
@@ -0,0 +1,7 @@
+namespace Beyond.NET.Sample.Source;
+
+// TODO: Need to fix init-only properties: https://github.com/royalapplications/beyondnet/issues/55
+
+// public record TestRecord(string AString)
+// {
+// }
