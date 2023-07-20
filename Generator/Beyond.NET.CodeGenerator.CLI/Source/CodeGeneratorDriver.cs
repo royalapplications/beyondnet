b54c68b059184018d3b35eda0744e7204ab676ce
Felix Deimel <felix@lemonmojo.com>
2023-07-20 10:38:19 +0200

    [Builder] Allow overwriting files in products dir

--- Generator/Beyond.NET.CodeGenerator.CLI/Source/CodeGeneratorDriver.cs
+++ Generator/Beyond.NET.CodeGenerator.CLI/Source/CodeGeneratorDriver.cs
@@ -335,14 +335,23 @@ internal class CodeGeneratorDriver
         // Get the files in the source directory and copy to the destination directory
         foreach (FileInfo file in dir.GetFiles()) {
             string targetFilePath = Path.Combine(destinationDir, file.Name);
-            file.CopyTo(targetFilePath);
+            
+            file.CopyTo(
+                targetFilePath,
+                true
+            );
         }
 
         // If recursive and copying subdirectories, recursively call this method
         if (recursive) {
             foreach (DirectoryInfo subDir in dirs) {
                 string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
-                CopyDirectory(subDir.FullName, newDestinationDir, true);
+                
+                CopyDirectory(
+                    subDir.FullName,
+                    newDestinationDir,
+                    true
+                );
             }
         }
     }
