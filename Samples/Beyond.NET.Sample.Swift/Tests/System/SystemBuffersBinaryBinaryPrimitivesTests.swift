01df036023dfdb2dfff5fd8fbddd0a70bcb6d592
Felix Deimel <felix@lemonmojo.com>
2023-11-17 16:36:38 +0100

    Add Tests for System.Buffers.Binary.BinaryPrimitives

---
+++ Samples/Beyond.NET.Sample.Swift/Tests/System/SystemBuffersBinaryBinaryPrimitivesTests.swift
@@ -0,0 +1,57 @@
+import XCTest
+import BeyondDotNETSampleKit
+
+final class SystemBuffersBinaryBinaryPrimitivesTests: XCTestCase {
+    @MainActor
+    override class func setUp() {
+        Self.sharedSetUp()
+    }
+    
+    @MainActor
+    override class func tearDown() {
+        Self.sharedTearDown()
+    }
+    
+    func testReadInt64() {
+        let value = Int64.max
+        let valueData = value.data
+        
+        guard let valueRet = try? System.Buffers.Binary.BinaryPrimitives.readInt64LittleEndian(valueData) else {
+            XCTFail("System.Buffers.Binary.BinaryPrimitives.ReadInt64LittleEndian should not throw and return an instance")
+            
+            return
+        }
+        
+        XCTAssertEqual(value, valueRet)
+    }
+    
+    func testTryReadUInt32() {
+        let value = UInt32.max
+        let valueData = value.data
+        
+        var valueRet: UInt32 = .min
+        
+        guard (try? System.Buffers.Binary.BinaryPrimitives.tryReadUInt32LittleEndian(valueData, &valueRet)) ?? false else {
+            XCTFail("System.Buffers.Binary.BinaryPrimitives.TryReadUInt32LittleEndian should not throw and return an instance")
+            
+            return
+        }
+        
+        XCTAssertEqual(value, valueRet)
+    }
+}
+
+
+fileprivate extension Int64 {
+    var data: Data {
+        var value = self
+        return Data(bytes: &value, count: MemoryLayout<Int>.size)
+    }
+}
+
+fileprivate extension UInt32 {
+    var data: Data {
+        var value = self
+        return Data(bytes: &value, count: MemoryLayout<UInt32>.size)
+    }
+}
