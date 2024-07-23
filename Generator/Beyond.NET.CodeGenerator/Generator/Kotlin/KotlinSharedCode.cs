11f2f3ad9396601c0ba17228bba60dc26275bd43
Felix Deimel <felix@lemonmojo.com>
2024-07-23 10:07:40 +0200

    [Kotlin] Bring DNObject to shared code

--- Generator/Beyond.NET.CodeGenerator/Generator/Kotlin/KotlinSharedCode.cs
+++ Generator/Beyond.NET.CodeGenerator/Generator/Kotlin/KotlinSharedCode.cs
@@ -2,5 +2,34 @@ namespace Beyond.NET.CodeGenerator.Generator.Kotlin;
 
 internal static class KotlinSharedCode
 {
-    internal const string SharedCode = "";
+    internal const string SharedCode = """
+open class DNObject(handle: Pointer) {
+   enum class DestroyMode {
+       Normal,
+       Skip
+   }
+
+   val handle: Pointer
+   var destroyMode: DestroyMode = DestroyMode.Normal
+
+   init {
+       require(handle !== Pointer.NULL) {
+           "Cannot initialize DNObject with a null pointer"
+       }
+
+       this.handle = handle
+   }
+
+   protected fun finalize() {
+       when (destroyMode) {
+           DestroyMode.Normal -> destroy()
+           DestroyMode.Skip -> {}
+       }
+   }
+
+   protected open fun destroy() {
+       // Override in subclass
+   }
+}
+""";
 }
--- Samples/Generated/Generated_Kotlin.kt
+++ Samples/Generated/Generated_Kotlin.kt
@@ -10,7 +10,34 @@ import com.sun.jna.ptr.*
 // MARK: - END Header
 
 // MARK: - BEGIN Utils
+open class DNObject(handle: Pointer) {
+   enum class DestroyMode {
+       Normal,
+       Skip
+   }
 
+   val handle: Pointer
+   var destroyMode: DestroyMode = DestroyMode.Normal
+
+   init {
+       require(handle !== Pointer.NULL) {
+           "Cannot initialize DNObject with a null pointer"
+       }
+
+       this.handle = handle
+   }
+
+   protected fun finalize() {
+       when (destroyMode) {
+           DestroyMode.Normal -> destroy()
+           DestroyMode.Skip -> {}
+       }
+   }
+
+   protected open fun destroy() {
+       // Override in subclass
+   }
+}
 
 
 
