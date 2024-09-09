47089df4c84544d30f1770d49ca31c7f70a9f202
Felix Deimel <felix@lemonmojo.com>
2024-09-09 16:37:19 +0200

    [kotlin] Nullable Pointer Playground

--- Samples/Beyond.NET.Sample.Android/app/src/androidTest/java/com/example/beyondnetsampleandroid/NullabilityTests.kt
+++ Samples/Beyond.NET.Sample.Android/app/src/androidTest/java/com/example/beyondnetsampleandroid/NullabilityTests.kt
@@ -8,11 +8,47 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 import com.example.beyondnetsampleandroid.dn.*
+import com.sun.jna.Pointer
+
+interface IDNObjectTest {
+    val __handle: Pointer
+}
+
+class DNObjectTest(handle: Pointer): IDNObjectTest {
+    override val __handle: Pointer
+
+    init {
+        require(handle !== Pointer.NULL) {
+            "Cannot initialize DNObject with a null pointer"
+        }
+
+        this.__handle = handle
+    }
+}
+
+@JvmName("getHandleOrNullPointer_optional")
+fun IDNObjectTest?.getHandleOrNullPointer(): Pointer {
+    if (this == null) {
+        val nullPtr = Pointer.NULL
+
+        // TODO: This throws: java.lang.NullPointerException: NULL must not be null
+        return nullPtr
+    }
+
+    return this.__handle
+}
 
 @RunWith(AndroidJUnit4::class)
 class NullabilityTests {
     @Test
     fun testMethodPropertyAndFieldNullability() {
+        // TODO: This is a playground for Pointer nullability
+        val nullObj: DNObjectTest? = null
+        val nullPtr = nullObj.getHandleOrNullPointer()
+
+
+
+        // These are the real tests
         val test = Beyond_NET_Sample_NullabilityTests()
 
         val helloString = "Hello".toDotNETString()
